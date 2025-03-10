45290782c90f0f873b2989451c1d454b1086c9be
Serhii Serdiuk <130998461+SerhiiSerdiukDev@users.noreply.github.com>
2025-03-10 17:31:21 +0200

    Update assembly information for WinSpeechToText

--- WinSpeechToText/Properties/AssemblyInfo.cs
+++ WinSpeechToText/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinWhisper")]
+[assembly: AssemblyTitle("WinSpeechToText")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinWhisper")]
+[assembly: AssemblyProduct("WinSpeechToText")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
